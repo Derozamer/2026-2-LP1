a1c5aa1ce7373279d7ac3ebc91cbbd82647ffd3a
Derozamer <abraham10feb2017@gmail.com>
2026-09-16 19:55:53 -0500

    Delete sem03-2 directory

--- sem03-2/aniobisiesto.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(void){
-    int anio = 2028;
-    if( anio  % 4 == 0 && (anio % 100 != 0 || anio % 400 == 0)){
-        printf("El anio es bisiesto!!!\n");
-    } else{
-        printf("El anio no es bisiesto :C\n");
-    }
-}
